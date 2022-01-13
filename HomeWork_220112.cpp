33bc62d6e5e8017592091d2fa88e78f9f8f44e36
tkdwls789 <97006993+tkdwls789@users.noreply.github.com>
2022-01-13 12:02:04 +0900

    Add files via upload

---
+++ HomeWork_220112.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+
+int main()
+{
+
+	int ArrJ[10] = {};
+	int ArrH[10] = {};
+
+	int IndexJ = 0;
+	int IndexH = 0;
+
+	while (10 != IndexJ)
+	{
+		ArrJ[IndexJ] =  2 * (IndexJ + 1);
+		IndexJ += 1;
+	}
+
+	while (10 != IndexH)
+	{
+		ArrH[IndexH] = 1 + IndexH * 2;
+		IndexH += 1;
+
+	}
+
+
+}
